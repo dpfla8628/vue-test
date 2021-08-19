e6c4a5145f3a01a34b3438330b742830af70549d
leeyerim <72377237+dpfla8628@users.noreply.github.com>
2021-08-20 00:00:52 +0900

    Update and rename 인푹 to 인풋박스 추가 제거 .vue

---
+++ 인풋박스 추가 제거 .vue
@@ -0,0 +1,37 @@
+<template>
+  <div class="AddRemove">
+    <div>
+      <ul>
+        <li v-for="(row, index) in rows">
+          <p>name : <input type="text" /></p>
+          <p>job : <input type="text" /></p>
+          <a @click="removeRow(row)">Remove</a>
+        </li>
+      </ul>
+      <div>
+        <button class="button btn-primary" @click="addRow">Add row</button>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import Axios from "axios";
+
+export default {
+  data() {
+    return {
+      rows: [{ name: "", job: "" }]
+    };
+  },
+  methods: {
+    addRow: function() {
+      this.rows.push({ name: "", job: "" });
+    },
+    removeRow: function(row) {
+      this.rows.$remove(row);
+    }
+  },
+  computed: {}
+};
+</script>
